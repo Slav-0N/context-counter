02e96c7d88523508dce6c15d904c7797066b4737
Slav-0N <116338599+Slav-0N@users.noreply.github.com>
2024-06-28 21:49:31 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+// import "./index.css";
 import { MyProvider } from "./components/context/context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
